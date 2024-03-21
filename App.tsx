b8aa57723fef33d6eac7767297fcea2443241f08
Mert Kaan İrtegün <irtegun16@itu.edu.tr>
2024-03-22 02:36:05 +0300

    Remove unused import and fix typo in tailwind.config.js

--- App.tsx
+++ App.tsx
@@ -2,7 +2,6 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import AppNavigator from './src/navigations/AppNavigator';
-import { SafeAreaView } from 'react-native';
 
 export default function App() {
   return (
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./App.{js,jsx,ts,tsx}", "./src/**/*.{js,jsx,ts,tsx}"],
-  newTheme: {
+  theme: {
     extend: {
       colors: {
         primary: {
